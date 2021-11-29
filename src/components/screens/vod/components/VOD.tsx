364db41745c9bae0aaa256c00b2bcece2f9be58c
Pierre Janineh <janinehpierre@gmail.com>
2021-11-29 12:24:47 +0200

    Menu data implementation

--- src/components/screens/vod/components/VOD.tsx
+++ src/components/screens/vod/components/VOD.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import VideoPlayer from '../../../../features/vod/components/VideoPlayer';
 import VideoHeader from '../../../../features/vod/components/VideoHeader';
 import CollapsibleMenu from '../../../../features/vod/components/CollapsibleMenu';
+import * as reducer from '../../../../features/menu/reducers/vodCategories.reducer';
+import { useSelector } from 'react-redux';
 
 const VOD = () => {
+  const categories = useSelector(reducer.selectVODCategories);
+
   return (
     <View style={styles.container}>
       <VideoPlayer />
       <VideoHeader />
-      <CollapsibleMenu />
+      <CollapsibleMenu categories={categories} />
     </View>
   );
 };
--- src/features/vod/components/CollapsibleMenu.tsx
+++ src/features/vod/components/CollapsibleMenu.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import CollapsedArrowSVG from '../../../../assets/svg/CollapsedArrowSVG';
 import * as Colors from '../../../constants/colors';
+import { VODCategory } from '../../../constants/types';
 
-const CollapsibleMenu = () => {
+const CollapsibleMenu = (props: { categories: VODCategory[] }) => {
+  const renderMenuItem = (item: { item: VODCategory }) => {
+    return <Text style={styles.text}>{item.item.name}</Text>;
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>כדורגל עולמי</Text>
+      <Text style={styles.text}>{props.categories[0].name}</Text>
       <View style={styles.icn}>
         <CollapsedArrowSVG collapsed={true} />
       </View>
+      <FlatList data={props.categories} renderItem={renderMenuItem} />
     </View>
   );
 };
