170e6c8d2b67c94183c95109acafb6b073744879
Pierre Janineh <janinehpierre@gmail.com>
2021-11-29 12:25:34 +0200

    Menu data implementation

--- App.tsx
+++ App.tsx
@@ -8,10 +8,12 @@ import { store } from './src/constants/store';
 import SplashScreen from 'react-native-splash-screen';
 import Navigator from './src/components/Navigator';
 import { StatusBar } from 'react-native';
+import * as reducer from './src/features/menu/reducers/vodCategories.reducer';
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
+    reducer.getVODCategories();
   }, []);
 
   return (
