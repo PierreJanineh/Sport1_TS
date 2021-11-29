f362d024f2086e41d4617d7e55ad66fa3d46063c
Pierre Janineh <janinehpierre@gmail.com>
2021-11-29 15:46:02 +0200

    Loading & Error components addition

---
+++ src/features/vod/components/ErrorView.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Strings from '../../../constants/strings';
+import * as Colors from '../../../constants/colors';
+
+const ErrorView = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>{Strings.vod.error}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: Colors.BKGD_GREY,
+  },
+});
+
+export default ErrorView;
